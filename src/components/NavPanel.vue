6c52abea1e75d5264330317189211d70af0ae661
rahim einollahi <r.einollahi@gamil.com>
2022-04-18 12:02:08 +0430

    Project done

--- src/components/NavPanel.vue
+++ src/components/NavPanel.vue
@@ -1,6 +1,35 @@
 <template>
     <div>
-        <h1>start create panel</h1>
+        <ul class="nav nav-tabs p-3">
+            <li class="nav-item">
+                <a href="#" class="nav-link">Features</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Sample</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Formats</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Stores</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Guidline</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Tariffs</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"> | 09147861734</a>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" to="/login-page">login</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class=" bg-info nav-link" to="/about">login</router-link>
+            </li>
+        </ul>
+        <!-- <button class="btn btn-info">Let's get started</button> -->
     </div>
 </template>
 
